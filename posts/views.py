7db097112832e741941636b50ba876583634efd8
rivka-yuliia <juliacreadora@gmail.com>
2023-06-26 19:23:55 +0300

    home

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, TemplateView
 
-# Create your views here.
+
+class Home(TemplateView):
+    template_name = 'posts/home.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+        context['title'] = 'Travelling blog - Home'
+        return context
--- travel_blog_project/settings.py
+++ travel_blog_project/settings.py
@@ -117,8 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = ['travel_blog_project/static']
 
 # Media files
 
