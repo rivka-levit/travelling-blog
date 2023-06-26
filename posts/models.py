cc59bbc5b1f9365986cf4798eacba205cb85a427
rivka-yuliia <juliacreadora@gmail.com>
2023-06-26 20:05:35 +0300

    post model

--- posts/models.py
+++ posts/models.py
@@ -12,3 +12,17 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=50, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
+    excerpt = models.CharField(max_length=255, blank=True)
+    image = models.ImageField(upload_to='post_photos')
+    text = models.TextField(max_length=5000, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
