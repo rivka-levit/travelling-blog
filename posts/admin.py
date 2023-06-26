75ce4149d8ae5bb40ac718151413c8bf20ea2783
rivka-yuliia <juliacreadora@gmail.com>
2023-06-26 20:14:23 +0300

    admin Post

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Post
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -7,4 +7,11 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'created_at', 'updated_at')
+    list_filter = ('category',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
