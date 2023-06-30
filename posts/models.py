394b9b1040d4855430c44507d23673710b88b192
rivka-yuliia <juliacreadora@gmail.com>
2023-06-30 06:08:28 +0300

    author is_owner

--- posts/models.py
+++ posts/models.py
@@ -20,6 +20,7 @@ class Category(models.Model):
 class Author(models.Model):
     name = models.CharField(max_length=50)
     photo = models.ImageField(upload_to='author_photos', null=True, blank=True)
+    is_owner = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
