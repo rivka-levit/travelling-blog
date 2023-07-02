82a1cb2bdf968311384be75dbb55f4c689832c1c
rivka-yuliia <juliacreadora@gmail.com>
2023-07-02 19:30:08 +0300

    subscriber model

--- mails/models.py
+++ mails/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Subscriber(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.name} ({self.email})'
