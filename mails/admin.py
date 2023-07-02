59fe1186458c41831ffaa224bea7ef2a9591718d
rivka-yuliia <juliacreadora@gmail.com>
2023-07-02 20:05:34 +0300

    admin for subscriber

--- mails/admin.py
+++ mails/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Subscriber
 
-# Register your models here.
+
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ['email', 'name', 'is_active']
+    list_display_links = ['email', 'name']
+    list_editable = ['is_active']
+    list_filter = ['is_active']
+
+
+admin.site.register(Subscriber, SubscriberAdmin)
