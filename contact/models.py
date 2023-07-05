8707de140c801a629b6e470790caaea0bf5ce850
rivka-yuliia <juliacreadora@gmail.com>
2023-07-06 00:55:44 +0300

    contact message model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactMessage(models.Model):
+    sender_name = models.CharField(max_length=50)
+    sender_email = models.EmailField()
+    text = models.TextField(max_length=500)
+    processed = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Contact message'
+        verbose_name_plural = 'Contact messages'
+
+    def __str__(self):
+        return f'{self.sender_name} ({self.sender_email})'
