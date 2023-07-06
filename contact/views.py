8fb629fb27507e57edf06cadfa1252676b62ff52
rivka-yuliia <juliacreadora@gmail.com>
2023-07-06 14:47:41 +0300

    post view contact

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from .forms import ContactMessageForm
+from .models import ContactMessage
 
 
 class ContactView(View):
@@ -9,3 +10,15 @@ class ContactView(View):
         return render(request, 'contact/contact.html', {
             'form': form
         })
+
+    def post(self, request):
+        form = ContactMessageForm(request.POST)
+        if form.is_valid():
+            new_message = ContactMessage(
+                sender_name=form.cleaned_data['sender_name'],
+                sender_email=form.cleaned_data['sender_email'],
+                text=form.cleaned_data['text']
+            )
+            new_message.save()
+            return redirect('home')
+        return redirect(request.META['HTTP_REFERER'])
